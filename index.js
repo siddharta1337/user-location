b50ac08f66fa9421b50576b7cd0124fa88806f8c
carlos solis <carlossolis@iMac.local>
2022-05-08 04:58:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const englishCode = "en-US";
+const spanishCode = "es-ES";
+function getAboutUsLink(language){
+    switch (language.toLowerCase()){
+      case englishCode.toLowerCase():
+        return '/about-us';
+      case spanishCode.toLowerCase():
+        return '/acerca-de';
+    }
+    return '';
+}
+module.exports = getAboutUsLink;
